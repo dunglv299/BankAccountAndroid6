70c2b207c75c72584acca9ce5cfc86cd32bc24ff
dunglv299 <dunglv90@gmail.com>
2013-07-18 14:00:26 +0700

    setup

---
+++ test/com/example/bankaccounttest/BankAccountDaoTest.java
@@ -0,0 +1,15 @@
+package com.example.bankaccounttest;
+
+import com.qsoft.bankaccount.BankAccountDao;
+
+import android.test.AndroidTestCase;
+
+public class BankAccountDaoTest extends AndroidTestCase {
+	private BankAccountDao bankAccountDao;
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+}
